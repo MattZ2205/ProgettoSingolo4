a4dbdcf5f718c9f916a83e0d2eb952f31e5838ff
Matteo Barchi <mat.barchi01@gmail.com>
2023-07-05 22:07:47 +0200

    Push rapido

--- Assets/Scripts/AIState.cs
+++ Assets/Scripts/AIState.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class AIState : MonoBehaviour
 {
@@ -10,5 +14,38 @@ public class AIState : MonoBehaviour
         Walking,
         Filling,
         Sleeping
+    }
+
+    public enum Event
+    {
+        Enter,
+        Updating,
+        Exit
+    }
+
+    public State action;
+    protected Event state;
+
+    protected NavMeshAgent agent;
+    protected AIState nextState;
+    protected Transform[] points;
+
+    public virtual void Enter() => state = Event.Updating;
+
+    public virtual void Updating() => state = Event.Updating;
+
+    public virtual void Exit() => state = Event.Exit;
+
+    public AIState Process()
+    {
+        if (state == Event.Enter) Enter();
+        else if (state == Event.Updating) Updating();
+        else if (state == Event.Exit)
+        {
+            Exit();
+            return nextState;
+        }
+
+        return this;
     }
 }
